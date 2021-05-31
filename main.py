d987586bbaedef5b880c9d3c8c7cc48b7a9f7da1
alex <alex.thunder@tut.by>
2021-05-31 12:01:23 +0000

    added 2nd project

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 import subprocess, re
 
+project_names = ['mycandidate', 'exclusivespecialist']
+
 def escape_ansi(line): # удалить из вывода ANSI-коды (например, которые меняют цвет текста в терминале)
     ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
     return ansi_escape.sub('', line)
@@ -13,16 +15,23 @@ def normalize_output(subprocess_result): # разбить вывод на спи
     normalized_output = list(filter(None, escape_ansi(subprocess_result).split('\n')))
     return normalized_output
 
-@app.route('/deploy_trigger')
-def deploy_trigger():
-    commands = [
-        # 'cd /ess_data/mycandidate/deployment && git pull',
-        'cd /ess_data/mycandidate/deployment && sudo docker-compose down',
-        'cd /ess_data/mycandidate/deployment && sudo docker-compose pull', 
-        'true && yes | sudo docker image prune',
-        'cd /ess_data/mycandidate/deployment && sudo docker-compose up -d'
-        ]
-    results = [normalize_output(subprocess.getoutput(command)) for command in commands]
+def deploy_trigger(project_name):
+    if project_name in project_names:
+        commands = [
+            # f'cd /ess_data/{project_name}/deployment && git pull',
+            f'cd /ess_data/{project_name}/deployment && sudo docker-compose down',
+            f'cd /ess_data/{project_name}/deployment && sudo docker-compose pull', 
+            f'true && yes | sudo docker image prune',
+            f'cd /ess_data/{project_name}/deployment && sudo docker-compose up -d'
+            ]
+        results = [normalize_output(subprocess.getoutput(command)) for command in commands]
+        return results
+    else:
+        return 'No such project'
+
+@app.route('/deploy/<project_name>')
+def deploy(project_name):
+    results = deploy_trigger(project_name)
     return {'output': [results]}
 
 if __name__ == '__main__':
